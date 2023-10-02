fcc3565068167e642caafc518c3d56a6337b2ee4
Guilherme Filipe Da Rosa <guilhermefdarosa15@gmail.com>
2023-10-02 17:18:08 -0300

    first commit

---
+++ src/views/birthday.vue
@@ -0,0 +1,48 @@
+<template>
+    <img src="../assets/marFoto.jpg" class="foto" />
+    <div>
+        <span>Tempo para meu aniversário:</span>
+        <br />
+        <span>{{ texto }}</span>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'birthday',
+    data() {
+        return {
+            texto: 'Tempo',
+        };
+    },
+    created() {
+        const dataFinal = new Date("2023-10-07T00:00:01").getTime();
+        const interval = setInterval(() => {
+            let now = new Date().getTime();
+            let diferenca = dataFinal - now;
+            let days = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+            let hours = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let minutes = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((diferenca % (1000 * 60)) / 1000);
+
+            this.texto = days + "d "
+                + hours + "h " + minutes + "m " + seconds + "s ";
+
+            if (diferenca < 0) {
+                clearInterval(interval);
+
+                this.texto = 'Chegouuuu!!!! aoooooo treeem!!!';
+            }
+        }, 1000);
+    },
+})
+</script>
+
+
+<style scoped>
+.foto {
+    height: 300px;
+}
+</style>
